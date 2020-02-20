62fdfc675669a6a5edd72825381e2330205e12c2
le0_0 <leonard.schoyen@gmail.com>
2020-02-20 22:20:26 +0100

    Task 2 a, b, c, d

---
+++ oving3/task2.sql
@@ -0,0 +1,20 @@
+﻿SELECT FilmID, Tittel, Produksjonsår, RegissørID FROM film;
+
+SELECT Navn FROM skuespiller
+    WHERE 1960 < Fødselsår;
+
+SELECT Navn FROM skuespiller
+    WHERE 1979 < Fødselsår AND Fødselsår < 1990
+    ORDER BY Navn;
+
+SELECT Tittel, Rolle FROM
+    (film JOIN
+        (SELECT FilmID, Rolle FROM
+            (skuespillerifilm JOIN
+                (SELECT SkuespillerID FROM skuespiller
+                    WHERE Navn = 'Morgan Freeman'
+                ) AS morgan ON skuespillerifilm.SkuespillerID = morgan.SkuespillerID
+            )
+        ) AS filmrolle ON film.FilmID = filmrolle.FilmID
+    )
+;
