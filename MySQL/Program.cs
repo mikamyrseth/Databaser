e841fd5a4a89e72c1f21c5ec11a606965a856842
le0_0 <leonard.schoyen@gmail.com>
2020-02-23 16:58:28 +0100

    Example program

--- MySQL/Program.cs
+++ MySQL/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
 namespace MySQL
 {
 
   class Program
   {
 
-    static void Main(string[] args) { Console.WriteLine("Hello World!"); }
+    static void Main(string[] args) {
+      string connStr = "server=localhost;user=root;database=mysql;port=3306;password=root";
+      MySqlConnection conn = new MySqlConnection(connStr);
+      try {
+        Console.WriteLine("Connecting to MySQL...");
+        conn.Open();
+        
+        string sql = "SELECT * FROM film;";
+        MySqlCommand cmd = new MySqlCommand(sql, conn);
+        MySqlDataReader rdr = cmd.ExecuteReader();
+
+        while (rdr.Read())
+        {
+          Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+        }
+        rdr.Close();
+      } catch (Exception ex) {
+        Console.WriteLine(ex.ToString());
+      } finally {
+        conn.Close();
+        Console.WriteLine("Done.");
+      }
+    }
 
   }
 
